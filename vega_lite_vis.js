16d64d2f591fad682e55bebe739508038412aafe
thew0006 <thew0006@student.monash.edu>
2026-05-20 20:59:14 +0800

    Added the created graphs from the other folder

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,7 +3,10 @@
 const chartConfigs = [
   { id: "chart-world-map", spec: "charts/world_map.vg.json" },
   { id: "chart-top-countries", spec: "charts/top_countries.vg.json" },
-  { id: "genre-tides", spec: "charts/genre_tides.vg.json" },
+  { id: "chart-genre-tides", spec: "charts/genre_tides.vg.json" },
+  { id: "chart-films-each-year", spec: "charts/films_each_year.vg.json" },
+  { id: "chart-genre-shape", spec: "charts/genre_shape.vg.json" },
+  { id: "chart-languages-of-cinema", spec: "charts/languages_of_cinema.vg.json" }
 ];
 
 // Embed each chart with shared options
