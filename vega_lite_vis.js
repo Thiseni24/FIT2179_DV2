a1c3dbe4cf67362fda937dc3853fdce2066cb4f8
thew0006 <thew0006@student.monash.edu>
2026-05-31 11:26:38 +0800

    changed the html code to structure the webpage better

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,14 +2,14 @@
 
 const chartConfigs = [
   { id: "chart-world-map", spec: "charts/world_map.vg.json" },
-  { id: "chart-genre-tides", spec: "charts/genre_tides.vg.json" },
   { id: "chart-films-each-year", spec: "charts/films_each_year.vg.json" },
-  { id: "chart-genre-shape", spec: "charts/genre_shape.vg.json" },
-  { id: "chart-languages-of-cinema", spec: "charts/languages_of_cinema.vg.json" },
-  { id: "chart-two-crowds", spec: "charts/two_crowds_taste.vg.json" },
+  { id: "chart-genre-tides", spec: "charts/genre_tides.vg.json" },
+  { id: "chart-language-bump", spec: "charts/language_bump_area.vg.json" },
+  { id: "chart-power-players", spec: "charts/power_players.vg.json" },
   { id: "chart-budget-revenue", spec: "charts/budget_vs_revenue.vg.json" },
   { id: "chart-rating-vs-reviews", spec: "charts/rating_vs_reviews.vg.json" },
-  { id: "chart-power-players", spec: "charts/power_players.vg.json" },
+  { id: "chart-two-crowds", spec: "charts/two_crowds_taste.vg.json" },
+  { id: "chart-genre-shape", spec: "charts/genre_shape.vg.json" },
   { id: "chart-genre-flow", spec: "charts/genre_chord.vg.json" },
   { id: "chart-mpa-heatmap", spec: "charts/mpa_heatmap.vg.json" }
 ];
